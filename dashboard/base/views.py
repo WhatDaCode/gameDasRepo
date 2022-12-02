86bfc9b59432ccd9df7b3f88d1954843243ea18b
NoLiferX <steliosgp13@gmail.com>
2022-12-02 03:17:50 +0200

    Add templates

--- dashboard/base/views.py
+++ dashboard/base/views.py
@@ -2,5 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(req):
-  return HttpResponse('Hello World')
+# Testing purposes only
+accounts = [
+    {
+        "id":"213123",
+        "summonerName":"ARNHTHS DRAKE",
+        "level":243,
+        "tier":"Bronze",
+        "rank":"I"
+    },
+    {
+        "id":"91919",
+        "summonerName":"Noliferx DRAKE",
+        "level":192,
+        "tier":"Silver",
+        "rank":"I"
+    }
+]
+
+def index(request):
+  context = {
+    'accounts' : accounts
+  }
+  return render(request, 'base/index.html', context)
+
+def about(request):
+  return render(request, 'base/about.html')
